b6ab5c0e5ce91ede0047af06fdd68661bb04969f
marcsue <marcsuejr@gmail.com>
2016-11-23 17:27:38 -0200

    controllerdpto

--- src/main/java/services/Controller.java
+++ src/main/java/services/Controller.java
@@ -6,68 +6,42 @@ import java.util.List;
 import objetos.Departamento;
 import objetos.Funcionario;
 
+import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import banco.DepartamentoDAO;
 import banco.FuncionarioDAO;
 
 @RestController
 public class Controller 
-{
+{	
+	//crud dpto
 
-	@RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "index";
-	}
-	
-	@RequestMapping(value="/departamento", method = RequestMethod.GET)
-	public boolean crud() throws ClassNotFoundException, SQLException
-	{
-		/*DepartamentoDAO dao = new DepartamentoDAO();
-		
-		Departamento dap = new Departamento(6,"seis");
-		
-		Departamento dpta = new Departamento(5,"xinco");
-		
-		dao.insereDepartamento(dpto);
-		dao.editarDepartamento(dpto, dpta);
-		dao.insereDepartamento(dap);*/
-		
-		Departamento dpto = new Departamento(5,"cinco");
-		
-		FuncionarioDAO dao = new FuncionarioDAO();
-		
-		Funcionario fun = new Funcionario(5, "joao", "lul", 21, 5, dpto);
-		Funcionario func = new Funcionario(5, "maria", "lul", 21, 12, dpto);
-		
-		
-		//dao.insereFuncionario(fun);
-		
-		
-	
-		return dao.editarFuncionario(fun, func);
+	@RequestMapping(value="/addDpto", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE )
+	public boolean addDpto(@RequestBody Departamento departamento) throws ClassNotFoundException 
+	{	
+		return new DepartamentoDAO().insereDepartamento(departamento);
 	}
 
-	@RequestMapping(value="/delete", method = RequestMethod.GET)
-	public boolean delete() throws ClassNotFoundException
+	
+	//deçete com erro
+	@RequestMapping(value="/deleteDpto", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE) 
+					
+	public boolean delete(@RequestBody Departamento departamento) throws ClassNotFoundException
 	{
-		FuncionarioDAO dao = new FuncionarioDAO();
-	
-	
-		return dao.deleteFuncionario(2);
+		
+		return new DepartamentoDAO().deleteDepartamento(departamento.getId());
 	}
 	
-	@RequestMapping(value="/busca", method = RequestMethod.GET)
-	public List<Funcionario> busca() throws ClassNotFoundException, SQLException
+	@RequestMapping(value="/buscaDpto", method = RequestMethod.GET)
+	public List<Departamento> busca() throws ClassNotFoundException, SQLException
 	{
-		FuncionarioDAO dao = new FuncionarioDAO();	
-		
-	
-		return dao.buscaFuncPorDep(5);
+		return new DepartamentoDAO().buscaTodos();
 	}
 	
 	
